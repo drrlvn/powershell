1ce4cf6096a1c5f0131bcf840f21a80e6d6f8879
Dror Levin <dror@psybear.com>
2020-12-31 12:27:38 +0100

    Initial commit

---
+++ profile.ps1
@@ -0,0 +1,23 @@
+function Install-RequiredModule {
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+    Install-Module Microsoft.PowerShell.ConsoleGuiTools
+    #Install-Module Microsoft.PowerShell.UnixCompleters
+    Install-Module -AllowClobber Get-ChildItemColor
+}
+
+Set-PSReadLineOption -HistoryNoDuplicates -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+# https://github.com/dotnet/runtime/issues/802
+#Set-PSReadLineKeyHandler -Key Alt+LeftArrow -Function BackwardWord
+#Set-PSReadLineKeyHandler -Key Alt+RightArrow -Function ForwardWord
+
+#Import-Module Microsoft.PowerShell.UnixCompleters
+
+Set-Alias -Name ls -Value Get-ChildItemColor -Option AllScope
+Set-Alias -Name rm -Value Remove-Item
+Set-Alias -Name cp -Value Copy-Item
+Set-Alias -Name mv -Value Move-Item
+
+Invoke-Expression (&starship init powershell)
