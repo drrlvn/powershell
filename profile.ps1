0a2a91f5e588a0115665b6ec38141406c88bf714
Dror Levin <dror@psybear.com>
2021-04-05 20:18:03 +0200

    Set pager, improve PSReadLine config

--- profile.ps1
+++ profile.ps1
@@ -1,13 +1,23 @@
 function Install-RequiredModule {
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
     Install-Module Microsoft.PowerShell.ConsoleGuiTools
+    Install-Module Terminal-Icons
     if (-not $IsWindows) {
         #Install-Module Microsoft.PowerShell.UnixCompleters
     }
-    Install-Module -AllowClobber Get-ChildItemColor
 }
 
-Set-PSReadLineOption -HistoryNoDuplicates -HistorySearchCursorMovesToEnd
+if (Get-Command 'less' -ErrorAction SilentlyContinue) {
+    $env:PAGER = 'less -RnXmiS'
+}
+
+$PSReadLineOptions = @{
+    EditMode                      = 'Emacs'
+    PredictionSource              = 'History'
+    HistoryNoDuplicates           = $true
+    HistorySearchCursorMovesToEnd = $true
+}
+Set-PSReadLineOption @PSReadLineOptions
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
@@ -16,13 +26,14 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 #Set-PSReadLineKeyHandler -Key Alt+RightArrow -Function ForwardWord
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function ViExit
 
+Import-Module Terminal-Icons
 if (-not $IsWindows) {
     #Import-Module Microsoft.PowerShell.UnixCompleters
 
-    Set-Alias -Name ls -Value Get-ChildItemColor -Option AllScope
-    Set-Alias -Name rm -Value Remove-Item
+    Set-Alias -Name ls -Value Get-ChildItem
     Set-Alias -Name cp -Value Copy-Item
     Set-Alias -Name mv -Value Move-Item
+    Set-Alias -Name rm -Value Remove-Item
 }
 
 Invoke-Expression (&starship init powershell)
